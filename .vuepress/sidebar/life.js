de6c46624e188126659b5e2b750f5e6d60b3632f
clinan <artergo163@gmail.com>
2020-02-22 17:21:51 +0800

    commit

--- .vuepress/sidebar/life.js
+++ .vuepress/sidebar/life.js
@@ -14,4 +14,11 @@ module.exports = [
             '/modules/life/2019_summary', // 你的md文件地址 
         ]
     },
+    {
+        title: '杂记，随思', // 侧边栏名称 
+        collapsable: true, // 可折叠 
+        children: [
+            '/modules/life/2020伊始', // 你的md文件地址 
+        ]
+    },
 ]
