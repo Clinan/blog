d4e87610ac549c6bc4f1d261e26ae49355b040e5
lachen <lachen@leqee.com>
2020-06-30 16:20:27 +0800

    qiniu

--- cdn.js
+++ cdn.js
@@ -2,7 +2,6 @@ const qiniu = require("qiniu");
 const proc = require("process");
 const path = require("path");
 const fs = require("fs");
-
 var bucket = proc.env.QINIU_BUCKET;
 var accessKey = proc.env.QINIU_ACCESS_KEY;
 var secretKey = proc.env.QINIU_SECRET_KEY;
@@ -19,47 +18,63 @@ var formUploader = new qiniu.form_up.FormUploader(config);
 // var putExtra = new qiniu.form_up.PutExtra();
 
 var __dirname = "./";
-var excludeSubDir=['node_modules','.git','dist','theme']
-var imageExtName = ['.jpg','.JPG','.PNG','.png','.JPEG','.ico','.jpeg']
+var excludeSubDir = ["node_modules", ".git", "dist", "theme"];
+var imageExtName = [".jpg", ".JPG", ".PNG", ".png", ".JPEG", ".ico", ".jpeg"];
 
-function upload(fullPath){
-    formUploader.putFile(uploadToken, path.basename(fullPath), fullPath, null, function (respErr,
-    respBody, respInfo) {
-    if (respErr) {
+function upload(fullPath) {
+  formUploader.putFile(
+    uploadToken,
+    path.basename(fullPath),
+    fullPath,
+    null,
+    function (respErr, respBody, respInfo) {
+      if (respErr) {
         throw respErr;
-    }
+      }
 
-    if (respInfo.statusCode == 200) {
+      if (respInfo.statusCode == 200) {
         console.log(respBody);
-    } else {
+      } else {
         console.log(respInfo.statusCode);
         console.log(respBody);
+      }
     }
+  );
+}
+
+function readFileList(dir, filesList = [], existFiles = []) {
+  const files = fs.readdirSync(dir);
+  // console.log(files);
+  files.forEach((item, index) => {
+    var fullPath = path.join(dir, item);
+    const stat = fs.statSync(fullPath);
+    if (stat.isDirectory() && !excludeSubDir.includes(item)) {
+      readFileList(path.join(dir, item), filesList); //递归读取文件
+    } else if (
+      !excludeSubDir.includes(item) &&
+      imageExtName.includes(path.extname(item)) &&
+      !existFiles.includes(path.extname(item))
+    ) {
+      upload(fullPath);
+      filesList.push(fullPath);
+    }
+  });
+  return filesList;
+}
+
+var bucketManager = new qiniu.rs.BucketManager(mac, config);
+bucketManager.listPrefix(bucket, null, function (respErr, respBody, respInfo) {
+  if (respErr) {
+    throw respErr;
+  }
+  if (respInfo.statusCode == 200) {
+    // console.log(respBody);
+    var existFiles = respBody.items.map((v) => v.key);
+    var filesList = [];
+    readFileList(__dirname, filesList, existFiles);
+    console.log(filesList.map((v) => path.basename(v)));
+  } else {
+    console.log(respInfo.statusCode);
+    console.log(respBody);
+  }
 });
-}
-
-function readFileList(dir, filesList = []) {
-    const files = fs.readdirSync(dir);
-    // console.log(files);
-    files.forEach((item, index) => {
-        var fullPath = path.join(dir, item);
-        const stat = fs.statSync(fullPath);
-        if (stat.isDirectory() && !excludeSubDir.includes(item)) {      
-            readFileList(path.join(dir, item), filesList);  //递归读取文件
-        } else if(!excludeSubDir.includes(item)&& imageExtName.includes(path.extname(item))){    
-            upload(fullPath)            
-            filesList.push(fullPath);                  
-        }        
-    });
-    return filesList;
-}
- 
-var filesList = [];
-readFileList(__dirname,filesList); 
-var filenamels = filesList.map(v=>path.basename(v));
-console.log(filenamels);
-
-
-
-
-
--- modules/photography/shaoxing.md
+++ modules/photography/shaoxing.md
@@ -5,31 +5,32 @@
 :::tip
 在《明朝那些事儿》中认识了徐渭，所以就特意去看了看。
 :::
-![](/photography/绍兴/青藤1.jpg)
-![](/photography/绍兴/青藤2.jpg)
-![](/photography/绍兴/青藤3.jpg)
-![](/photography/绍兴/青藤4.jpg)
-![](/photography/绍兴/青藤5.jpg)
-![](/photography/绍兴/青藤6.jpg)
-![](/photography/绍兴/青藤7.jpg)
-![](/photography/绍兴/青藤8.jpg)
+<!-- ![](/photography/绍兴/青藤1.jpg) -->
+![](http://cdn.clinan.xyz/青藤1.jpg)
+![](http://cdn.clinan.xyz/青藤2.jpg)
+![](http://cdn.clinan.xyz/青藤3.jpg)
+![](http://cdn.clinan.xyz/青藤4.jpg)
+![](http://cdn.clinan.xyz/青藤5.jpg)
+![](http://cdn.clinan.xyz/青藤6.jpg)
+![](http://cdn.clinan.xyz/青藤7.jpg)
+![](http://cdn.clinan.xyz/青藤8.jpg)
 
 ## 绍兴臭豆腐
-![](/photography/绍兴/臭豆腐.jpg)
+![](http://cdn.clinan.xyz/臭豆腐.jpg)
 
 
 ## 鲁镇 社戏
 :::tip
 社戏听不懂，没有字幕，很是可惜。最后还看到来著名的啊Q受审的情景剧，但是很快结束了，没有来得及留照片。
 :::
-![](/photography/绍兴/鲁镇1.jpg)
-![](/photography/绍兴/鲁镇2.jpg)
-![](/photography/绍兴/鲁镇3.jpg)
-![](/photography/绍兴/鲁镇4.jpg)
-![](/photography/绍兴/鲁镇5.jpg)
-![](/photography/绍兴/鲁镇6.jpg)
-![](/photography/绍兴/鲁镇7.jpg)
-![](/photography/绍兴/鲁镇8.jpg)
-![](/photography/绍兴/鲁镇9.jpg)
-![](/photography/绍兴/鲁镇10.jpg)
-![](/photography/绍兴/鲁镇11.jpg)
+![](http://cdn.clinan.xyz/鲁镇1.jpg)
+![](http://cdn.clinan.xyz/鲁镇2.jpg)
+![](http://cdn.clinan.xyz/鲁镇3.jpg)
+![](http://cdn.clinan.xyz/鲁镇4.jpg)
+![](http://cdn.clinan.xyz/鲁镇5.jpg)
+![](http://cdn.clinan.xyz/鲁镇6.jpg)
+![](http://cdn.clinan.xyz/鲁镇7.jpg)
+![](http://cdn.clinan.xyz/鲁镇8.jpg)
+![](http://cdn.clinan.xyz/鲁镇9.jpg)
+![](http://cdn.clinan.xyz/鲁镇10.jpg)
+![](http://cdn.clinan.xyz/鲁镇11.jpg)
