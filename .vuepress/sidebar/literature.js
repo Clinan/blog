55004a83d28f397b579982611debb7254c1e6da5
clinan <artergo163@gmail.com>
2020-07-12 22:10:12 +0800

    update

--- .vuepress/sidebar/literature.js
+++ .vuepress/sidebar/literature.js
@@ -1,11 +1,23 @@
-module.exports = [ 
-  '/modules/literature/',
-  // {
-  //     title: '现代诗词',
-  //     collapsable: true, 
-  //     children: [
-  //         '/modules/literature/poem',
-  //         '/modules/literature/muying',
-  //     ]
-  // },
+module.exports = [
+  // '/modules/literature/',
+  {
+    title: '现代诗词',
+    collapsable: true,
+    children: [
+      {
+        title: '花花的诗',
+        collapsable: true,
+        children: [
+          '/modules/literature/poem',
+        ]
+      },
+      {
+        title: '木莺',
+        collapsable: true,
+        children: [
+          '/modules/literature/muying',
+        ]
+      },
+    ]
+  },
 ]
