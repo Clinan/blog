108a4c48ff7786e0a47a912ac10e5b3cc6fce813
clinan <artergo163@gmail.com>
2021-01-09 21:36:49 +0800

    push

--- .vuepress/sidebar/photography.js
+++ .vuepress/sidebar/photography.js
@@ -2,6 +2,7 @@ module.exports = [
     '/modules/photography/',
     '/modules/photography/1207',
     '/modules/photography/shaoxing',
+    '/modules/photography/changsha',
     // {
     //     title: 'JavaScript', // 侧边栏名称 
     //     collapsable: true, // 可折叠 
